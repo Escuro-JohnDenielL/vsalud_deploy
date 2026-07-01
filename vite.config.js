1d29ed6a82739fee196d20cf947a4a452638660b
John Deniel Escuro <johndenielescuro@gmail.com>
2026-07-02 01:07:35 +0800

    john

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'node:fs';
+import path from 'node:path';
+
+const cssInputs = fs
+    .readdirSync(path.resolve('resources/css'))
+    .filter((file) => file.endsWith('.css'))
+    .map((file) => `resources/css/${file}`);
+
+const jsInputs = fs
+    .readdirSync(path.resolve('resources/js'))
+    .filter((file) => file.endsWith('.js'))
+    .map((file) => `resources/js/${file}`);
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/js/a_homepage.js',
-                'resources/js/a_inquiries.js',
-                'resources/js/a_profile.js',
-                'resources/js/a_report.js',
-                'resources/js/a_reserve.js',
-                'resources/js/faqs.js',
-                'resources/js/login.js',
-                'resources/js/p_homepage.js',
-                'resources/js/p_mreserve.js',
-                'resources/js/p_vreserve.js',
-                'resources/js/payment_order.js',
-                'resources/js/signup.js',
-            ],
+            input: [...cssInputs, ...jsInputs],
             refresh: true,
         }),
     ],
